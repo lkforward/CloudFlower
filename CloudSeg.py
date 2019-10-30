15a87f092c2a5ed103fcf819ca47e0bd1d7d755b
Ke <lkforward@gmail.com>
2019-10-30 09:40:00 -0400

    add predict()

--- CloudSeg.py
+++ CloudSeg.py
@@ -165,6 +165,41 @@ class CloudSeg(object):
 		pass
 
 	def predict(self, plot_samples=False):
+		##################################
+		# Make predictions:
+		#
+		# INPUTS: runner, class_params
+		##################################
+		import gc
+		torch.cuda.empty_cache()
+		gc.collect()
+		
+		runner = self.runner
+
+		# Load testing data: 
+		test_dataset = CloudDataset(df=sub, datatype='test', img_ids=test_ids, transforms = get_validation_augmentation(), preprocessing=get_preprocessing(preprocessing_fn))
+		test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False, num_workers=0)
+
+		loaders = {"test": test_loader}
+
+		# Visualize some predictions: 
+		encoded_pixels = []
+		image_id = 0
+		for i, test_batch in enumerate(tqdm.tqdm(loaders['test'])):
+		    runner_out = runner.predict_batch({"features": test_batch[0].cuda()})['logits']
+		    for i, batch in enumerate(runner_out):
+			for probability in batch:
+
+			    probability = probability.cpu().detach().numpy()
+			    if probability.shape != (350, 525):
+				probability = cv2.resize(probability, dsize=(525, 350), interpolation=cv2.INTER_LINEAR)
+			    predict, num_predict = post_process(sigmoid(probability), class_params[image_id % 4][0], class_params[image_id % 4][1])
+			    if num_predict == 0:
+				encoded_pixels.append('')
+			    else:
+				r = mask2rle(predict)
+				encoded_pixels.append(r)
+			    image_id += 1
 		pass
 
 
