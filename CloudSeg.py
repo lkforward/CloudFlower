daa343680567e27e4396a50c795a2847e9f4d80c
Ke <lkforward@gmail.com>
2019-10-30 08:32:15 -0400

    Create a model class to facilitate experiments

---
+++ CloudSeg.py
@@ -0,0 +1,16 @@
+class CloudSeg(object):
+	def __init__():
+		pass
+
+	def train(self):
+		pass
+
+	def select_param(self, plot_samples=False):
+		pass
+
+	def predict(self, plot_samples=False):
+		pass
+
+
+if __name__ == '__main__':
+	pass
